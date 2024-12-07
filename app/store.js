b3b8dbe01d73149bfbce6f4f91a6f597b1a80947
osaro imarhiagbe <osaroimarhiagbe@gmail.com>
2024-12-07 13:38:40 -0600

    updating file

--- app/store.js
+++ app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import socialReducer from './features/PostandComments/socialSlice'
-
+import userReducer from './features/User/userSlice';
 
 export default configureStore({
   reducer: {
-    social:socialReducer}
+    social:socialReducer,
+    user:userReducer
+  }
 })
